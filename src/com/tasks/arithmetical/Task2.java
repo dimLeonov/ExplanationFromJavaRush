7f8ee9a1470857871c427d6ebfbaba9877b4cd89
DLeonovs <dim.leonov97@gmail.com>
2019-04-10 18:40:34 +0300

    Second Commit

--- src/com/tasks/arithmetical/Task2.java
+++ src/com/tasks/arithmetical/Task2.java
@@ -4,6 +4,7 @@ public class Task2 {
     public static void main(String[] args) {
         System.out.println(convertEurToUsd(41,15));
         System.out.println(convertEurToUsd(42,12));
+        System.out.println(convertEurToUsd(100,10));
     }
 
     public static double convertEurToUsd(int eur, double course) {
